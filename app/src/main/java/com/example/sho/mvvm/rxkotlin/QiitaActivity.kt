73a38d5ede4dc9526f1a65dd3d4e55ce4b100516
ussy_free <sudo5in5k@gmail.com>
2018-12-02 22:56:51 +0900

    to qiita list fragment

--- app/src/main/java/com/example/sho/mvvm/rxkotlin/QiitaActivity.kt
+++ app/src/main/java/com/example/sho/mvvm/rxkotlin/QiitaActivity.kt
@@ -2,41 +2,17 @@ package com.example.sho.mvvm.rxkotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.example.sho.mvvm.R
-import com.example.sho.mvvm.rxkotlin.retrofit.QiitaClient
-import rx.Subscription
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 
 class QiitaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qiita)
-    }
 
-    override fun onResume() {
-        super.onResume()
-        subscribe()
-    }
-
-    private fun subscribe(): Subscription? {
-        return QiitaClient.client().items().subscribeOn(Schedulers.newThread())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ item ->
-                    item.forEach {
-                        Log.d("ushi_debug", it.title)
-                    }
-                }, { e ->
-                    Log.e("ushi_debug_error", e.message)
-                })
-    }
-
-    override fun onPause() {
-        super.onPause()
-        subscribe()?.unsubscribe()
-        Log.d("ushi_debug", "unsubscribe")
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.qiita_list_container, QiitaListFragment())
+                .addToBackStack(null)
+                .commit()
     }
 }
--- app/src/main/java/com/example/sho/mvvm/rxkotlin/QiitaListFragment.kt
+++ app/src/main/java/com/example/sho/mvvm/rxkotlin/QiitaListFragment.kt
@@ -1,25 +1,42 @@
 package com.example.sho.mvvm.rxkotlin
 
 import android.os.Bundle
-import android.support.v4.app.ListFragment
+import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.sho.mvvm.R
 import com.example.sho.mvvm.rxkotlin.retrofit.QiitaClient
-import com.example.sho.mvvm.rxkotlin.retrofit.QiitaItemResponse
-import rx.Subscriber
+import kotlinx.android.synthetic.main.fragment_qiita_list.*
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
-class QiitaListFragment : ListFragment() {
+class QiitaListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_qiita_list, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val adapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_activated_1)
+        subscribe(adapter)
+        qiita_list.adapter = adapter
+    }
+
+    private fun subscribe(adapter: ArrayAdapter<String>): Subscription? {
+        return QiitaClient.client().items().subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ item ->
+                    item.forEach {
+                        adapter.add(it.title)
+                    }
+                }, { e ->
+                    Log.e("ushi_debug_error", e.message)
+                })
     }
 }
